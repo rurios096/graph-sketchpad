06a27ed076a616c100665aaa42f47233f9e4cd3e
Rafael Uriostegui <rafael.uriostegui@wsu.edu>
2023-10-21 10:09:04 -0700

    Buttons style updated, link button to adding elements.

--- sketchpad.js
+++ sketchpad.js
@@ -1,21 +1,78 @@
 var graphLength = 0;
 const radius = 20;
 const adjacencyList = {};
+var vButtonToggle = false;
+var eButtonToggle = false;
 
 const svg = d3.select('#graph-container');
 var box = document.getElementById("graph-container");
+var vButtonState = document.getElementById("vButton");
+var eButtonState = document.getElementById("eButton");
+
 
 box.addEventListener("click", function (event) {
 
     var xCoord = event.clientX - box.getBoundingClientRect().left;
     var yCoord = event.clientY - box.getBoundingClientRect().top;
 
-    if (!findOverlap(xCoord, yCoord) && withinBox(xCoord, yCoord))
-    {
-        addVertex(xCoord, yCoord, graphLength);
-        ++graphLength;
+    if (!findOverlap(xCoord, yCoord) && withinBox(xCoord, yCoord)){
+
+        if(vButtonToggle)
+        {
+            addVertex(xCoord, yCoord, graphLength);
+            ++graphLength;
+        }   
     }
-        
+});
+
+vButtonState.addEventListener("click", function (event) {
+
+    var vButtonState = document.getElementById("vButton");
+
+    if(vButtonToggle) // not active/clicked
+    {
+        vButtonToggle = false;
+        vButtonState.style.backgroundColor = "white";
+        vButtonState.style.borderColor = "rgb(164, 164, 164)";
+        vButtonState.style.fontWeight = "100";
+    }
+    else{  // active/clicked
+        if(eButtonToggle)
+        {
+            eButtonToggle = true;
+            eButtonState.click();
+        }
+        vButtonToggle = true;
+        vButtonState.style.backgroundColor = "rgb(210, 210, 210)";
+        vButtonState.style.borderColor = "rgb(87, 87, 87)"
+        vButtonState.style.fontWeight = "1000";
+    }
+    
+});
+
+eButtonState.addEventListener("click", function (event) {
+
+    var eButtonState = document.getElementById("eButton");
+
+    if(eButtonToggle)  // not active/clicked
+    {
+        eButtonToggle = false;
+        eButtonState.style.backgroundColor = "white";
+        eButtonState.style.borderColor = "rgb(164, 164, 164)";
+        eButtonState.style.fontWeight = "100";
+    }
+    else{  // active/clicked
+        if(vButtonToggle)
+        {
+            vButtonToggle = true;
+            vButtonState.click();
+        }
+        eButtonToggle = true;
+        eButtonState.style.backgroundColor = "rgb(210, 210, 210)";
+        eButtonState.style.borderColor = "rgb(87, 87, 87)";
+        eButtonState.style.fontWeight = "1000";
+    }
+    
 });
 
 function findOverlap(x, y) {
