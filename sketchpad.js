a6d25a7dd3cff15e5244d9a2589153091e9c430d
Rafael Uriostegui <rafael.uriostegui@wsu.edu>
2023-10-24 21:08:56 -0700

    New feature: create edges.

--- sketchpad.js
+++ sketchpad.js
@@ -1,10 +1,13 @@
 var graphLength = 0;
+var edgeNum = 0;
 const radius = 23;
 const adjacencyList = {};
 var vButtonToggle = false;
 var eButtonToggle = false;
 var dButtonToggle = false;
 var selectedColor = "#008000";
+var foundVertex1 = false;
+var foundVertex2 = false;
 
 const svg = d3.select('#graph-container');
 var box = document.getElementById("graph-container");
@@ -24,23 +27,35 @@ box.addEventListener("click", function (event) {
 
     var xCoord = event.clientX - box.getBoundingClientRect().left;
     var yCoord = event.clientY - box.getBoundingClientRect().top;
+    console.log("x: " + xCoord + " y: " + yCoord)
 
-    if (!findOverlap(xCoord, yCoord) && withinBox(xCoord, yCoord)){
+    if (withinBox(xCoord, yCoord)){
 
-        if(vButtonToggle)
+        if(vButtonToggle && !findOverlap(xCoord, yCoord))
         {
             addVertex(xCoord, yCoord, graphLength);
             ++graphLength;
-        }   
+        }
+        else if(dButtonToggle)
+        {
+            var foundVertex = findOverlap(xCoord, yCoord);
+            if(foundVertex) deleteVertex(foundVertex); // found vertex
+        }
+        else if(eButtonToggle)
+        {
+            if(!foundVertex1){ // new/first
+                foundVertex1 = findOverlap(xCoord, yCoord);
+                return;
+            }
+
+            if (!foundVertex2){
+                foundVertex2 = findOverlap(xCoord, yCoord);
+            }
+
+            if(foundVertex1 && foundVertex2) addEdge(foundVertex1, foundVertex2); // found 2 vertices
+        }
     }
 });
-
-
-
-
-
-
-
 
 vButtonState.addEventListener("click", function (event) {
     changeButton(vButtonState, vButtonToggle);
@@ -50,6 +65,7 @@ vButtonState.addEventListener("click", function (event) {
 eButtonState.addEventListener("click", function (event) {
     changeButton(eButtonState, eButtonToggle);
     eButtonToggle = !eButtonToggle;
+    if(!eButtonToggle) foundVertex1 = foundVertex2 = false;
 });
 
 dButtonState.addEventListener("click", function (event) {
@@ -93,28 +109,26 @@ function unclickButtons(stateID){
         case "dButton":
             if(eButtonToggle) toInactiveButton(eButtonState);
             if(vButtonToggle) toInactiveButton(vButtonState);
-            eButtonToggle = eButtonToggle = false;
+            eButtonToggle = vButtonToggle = false;
             break;
       }
 }
 
-
-
-
-
-function findOverlap(x, y) {
+function findOverlap(x, y){
 
     const elements = box.querySelectorAll("*");
 
     for (const element of elements) {
-
         if('vertex' !== element.getAttribute('class')) continue;
 
-        const xCoord = parseFloat(element.getAttribute('cx'))
-        const yCoord = parseFloat(element.getAttribute('cy'))
+        const vertexGroup = d3.select(element);
+        const vertexShape = vertexGroup.select('.vertexShape');
+
+        const xCoord = parseFloat(vertexShape.attr('cx'))
+        const yCoord = parseFloat(vertexShape.attr('cy'))
 
         const distance = Math.sqrt((x - xCoord) ** 2 + (y - yCoord) ** 2);
-        if(distance < radius * 2) return true
+        if(distance < radius * 2) return vertexGroup.attr('id')
     }
     return false;
 }
@@ -128,38 +142,100 @@ function addVertex(x, y, num)
 {
     const vID = 'v' + num;
 
-    const vertexGroup = svg.append('g');
+    const vertexGroup = svg.append('g')
+        .attr('class', 'vertex')
+        .attr('id', vID);
 
     const vertexElement = vertexGroup.append('circle')
     .attr('cx', x)
     .attr('cy', y)
     .attr('r', radius)
-    .attr('class', 'vertex')
-    .attr('id', vID)
+    .attr('class', 'vertexShape')
     .style("cursor", "pointer")
     .style("fill", selectedColor);
 
     vertexGroup.append('text')
         .attr('x', x)
-        .attr('y', + y + 7)
+        .attr('y', y + 7)
         .text('v' + num)
         .attr('class', 'vertexLabel')
         .attr('text-anchor', 'middle')
         .style("fill", "white")
         .style("cursor", "pointer");
+     
 
-    /*vertexGroup
-        .on('mouseover', function () {
-            d3.select(this).transition()
-                .transition('100')
-                    .duration(800)
-                .attr('opacity', '.70')
-            })
-        .on("mouseout", function () {
-            d3.select(this).classed("hovered", false) // Remove the class on mouseout
-            d3.select(this).transition()
-                .attr('opacity', '1')
-        });
-        */      
-    adjacencyList[num] = [];
+    adjacencyList[vID] = [];
+
+}
+
+function getArcPoint(vx1, vy1, vx2, vy2)
+{
+    const xCoord1 = parseFloat(vx1);
+    const yCoord1 = parseFloat(vy1);
+    const xCoord2 = parseFloat(vx2);
+    const yCoord2 = parseFloat(vy2);
+
+    var angle1 = Math.atan2(yCoord2 - yCoord1, xCoord2 - xCoord1);
+    var angle2 = Math.atan2(yCoord1 - yCoord2 , xCoord1 - xCoord2);
+
+    const x1 = xCoord1 + radius * Math.cos(angle1)
+    const y1 = yCoord1 + radius * Math.sin(angle1)
+    const x2 = xCoord2 + radius * Math.cos(angle2)
+    const y2 = yCoord2 + radius * Math.sin(angle2)
+
+    const xMid = (x2 + x1) / 2;
+    const yMid = (y2 + y1) / 2;
+
+    var pathData = [
+        [x1,y1],
+        [xMid, yMid],
+        [x2,y2]
+    ];
+
+    return pathData
+}
+
+function addNeighbor(vertex, neighbor, edge){
+    // if (adjacencyList[vertex] includes([neighbor, any])) // parallel edge
+    // {
+    //     return;
+    // }
+    adjacencyList[vertex].push([neighbor, edge]);
+}
+
+function addEdge(id1, id2){
+
+    const eID = 'e' + edgeNum;
+    var vertexGroup1 = d3.select('#' + id1);
+    var vertexGroup2 = d3.select('#' + id2);
+
+    const vertexShape1 = vertexGroup1.select('.vertexShape');
+    const vertexShape2 = vertexGroup2.select('.vertexShape');
+
+    const pathData = getArcPoint(vertexShape1.attr('cx'), vertexShape1.attr('cy'), vertexShape2.attr('cx'), vertexShape2.attr('cy'));
+
+    const linkGen = d3.line()
+        .x(d => d[0])
+        .y(d => d[1])
+        .curve(d3.curveBasis);
+
+
+    const edge = svg.append('path')
+        .data([pathData])
+        .attr("d", linkGen)
+        .attr("id", eID)
+        .attr("fill", "none")
+        .attr("stroke", "black")
+        .attr("stroke-width", 2)
+        .attr('stroke-linecap', 'round');        
+    
+    ++edgeNum;
+    foundVertex1 = foundVertex2 = false;
+    addNeighbor(id1, id2, eID);
+    addNeighbor(id2, id1, eID);
+}
+
+function deleteVertex(id){
+    var vertexGroup = d3.select('#' + id);
+    vertexGroup.remove();
 }
