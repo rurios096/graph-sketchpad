790ecb6bcb4118ac26dd0692d0603a7340621a46
Rafael Uriostegui <rafael.uriostegui@wsu.edu>
2023-10-25 18:13:05 -0700

    New feature: dragging vertex/edge. Fixed deleteVertex.

--- sketchpad.js
+++ sketchpad.js
@@ -1,13 +1,13 @@
-var graphLength = 0;
-var edgeNum = 0;
 const radius = 23;
 const adjacencyList = {};
+var graphLength = 0;
+var edgeNum = 0;
 var vButtonToggle = false;
 var eButtonToggle = false;
 var dButtonToggle = false;
-var selectedColor = "#008000";
 var foundVertex1 = false;
 var foundVertex2 = false;
+var selectedColor = "#008000";
 
 const svg = d3.select('#graph-container');
 var box = document.getElementById("graph-container");
@@ -18,6 +18,51 @@ var dButtonState = document.getElementById("dButton");
 
 var colorBox = document.getElementById("colorPicker");
 
+const drag = d3.drag()
+    .on("start", dragStart)
+    .on("drag", draggingVertex)
+    .on("end", dragEnd);
+
+const linkGen = d3.line()
+        .x(d => d[0])
+        .y(d => d[1])
+        .curve(d3.curveCardinal.tension(0.1));
+
+function dragStart(event){
+    
+}
+
+function dragEnd(event){
+}
+
+function draggingVertex(event){
+
+    var xCoord = event.x;
+    var yCoord = event.y;
+
+    const draggedElement = d3.select(this);
+    if(draggedElement.attr('class') === 'vertex')
+    {
+        draggedShape = draggedElement.select('circle')
+        draggedLabel = draggedElement.select('text')
+    
+        draggedShape.attr("cx", xCoord);
+        draggedShape.attr("cy", yCoord);
+        draggedLabel.attr("x", xCoord);
+        draggedLabel.attr("y", yCoord + 7);
+    
+        if (adjacencyList[draggedElement.attr("id")] !== undefined) 
+        {
+            if(adjacencyList[draggedElement.attr('id')].length !== 0) updatePath(draggedElement.attr('id'));
+        }
+    }
+    else if(draggedElement.attr('class') === 'edge')
+    {
+        draggedElement.attr("cx", xCoord);
+        draggedElement.attr("cy", yCoord);
+        updateMidPath(draggedElement.attr('id').slice(0, -1), xCoord, yCoord);
+    }
+}
 colorBox.addEventListener("change", function(event)
 {
     selectedColor = event.target.value;
@@ -147,12 +192,12 @@ function addVertex(x, y, num)
         .attr('id', vID);
 
     const vertexElement = vertexGroup.append('circle')
-    .attr('cx', x)
-    .attr('cy', y)
-    .attr('r', radius)
-    .attr('class', 'vertexShape')
-    .style("cursor", "pointer")
-    .style("fill", selectedColor);
+        .attr('cx', x)
+        .attr('cy', y)
+        .attr('r', radius)
+        .attr('class', 'vertexShape')
+        .style("cursor", "pointer")
+        .style("fill", selectedColor);
 
     vertexGroup.append('text')
         .attr('x', x)
@@ -166,6 +211,39 @@ function addVertex(x, y, num)
 
     adjacencyList[vID] = [];
 
+    const draggableVertex = d3.select('#' + vID);
+    draggableVertex.call(drag)
+}
+
+function updateMidPath(id, x, y){
+
+    const edgePath = d3.select('#' + id);
+
+    const pathData = edgePath.data() // [ [x1,y1], [midx,midy], [x2,y2] ]
+    pathData[0][1] = [x, y]
+
+    edgePath.attr("d", linkGen(pathData[0]));
+}
+
+function updatePath(id){
+
+    const vGroup = d3.select('#' + id);
+    const vShape = vGroup.select('.vertexShape');
+
+    for(const nPair of adjacencyList[id]){
+        const nGroup = d3.select('#' + nPair[0]);
+        const nShape = nGroup.select('.vertexShape');
+
+        const pathData = getArcPoint(vShape.attr('cx'), vShape.attr('cy'), nShape.attr('cx'), nShape.attr('cy'));
+
+        var path = d3.select('#' + nPair[1])
+            .data([pathData])
+            .attr("d", linkGen(pathData)); 
+
+        var midPath = d3.select('#' + nPair[1] + 'm')
+            .attr('cx', pathData[1][0])
+            .attr('cy', pathData[1][1]);
+    }
 }
 
 function getArcPoint(vx1, vy1, vx2, vy2)
@@ -187,9 +265,9 @@ function getArcPoint(vx1, vy1, vx2, vy2)
     const yMid = (y2 + y1) / 2;
 
     var pathData = [
-        [x1,y1],
+        [x1.toFixed(2),y1.toFixed(2)],
         [xMid, yMid],
-        [x2,y2]
+        [x2.toFixed(2),y2.toFixed(2)]
     ];
 
     return pathData
@@ -201,11 +279,13 @@ function addNeighbor(vertex, neighbor, edge){
     //     return;
     // }
     adjacencyList[vertex].push([neighbor, edge]);
+    
 }
 
 function addEdge(id1, id2){
 
     const eID = 'e' + edgeNum;
+    const mID = eID + 'm';
     var vertexGroup1 = d3.select('#' + id1);
     var vertexGroup2 = d3.select('#' + id2);
 
@@ -214,28 +294,52 @@ function addEdge(id1, id2){
 
     const pathData = getArcPoint(vertexShape1.attr('cx'), vertexShape1.attr('cy'), vertexShape2.attr('cx'), vertexShape2.attr('cy'));
 
-    const linkGen = d3.line()
-        .x(d => d[0])
-        .y(d => d[1])
-        .curve(d3.curveBasis);
-
-
     const edge = svg.append('path')
         .data([pathData])
-        .attr("d", linkGen)
+        .attr("d", linkGen(pathData))
         .attr("id", eID)
         .attr("fill", "none")
         .attr("stroke", "black")
-        .attr("stroke-width", 2)
+        .attr("stroke-width", 3)
         .attr('stroke-linecap', 'round');        
     
+    const midEdge = svg.append('circle')
+        .attr('cx', pathData[1][0])
+        .attr('cy', pathData[1][1])
+        .attr('r', 4)
+        .attr('class', 'edge')
+        .attr("id", mID)
+        .style("cursor", "pointer")
+        .style("fill", 'black');
+
     ++edgeNum;
     foundVertex1 = foundVertex2 = false;
     addNeighbor(id1, id2, eID);
     addNeighbor(id2, id1, eID);
+    midEdge.call(drag)
 }
 
 function deleteVertex(id){
     var vertexGroup = d3.select('#' + id);
+
+    for (const vertex in adjacencyList){
+        var updatedList = [];
+
+        for(const pair of adjacencyList[vertex])
+        {
+            if (pair[0] !== id) // neighbor isn't vertex
+            {
+                updatedList.push(pair)
+            }
+            else{
+                var edge = d3.select('#' + pair[1]);
+                edge.remove();
+            }
+        }
+
+        adjacencyList[vertex] = updatedList;
+    }
+
+    delete adjacencyList[id];
     vertexGroup.remove();
 }
